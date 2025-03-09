ba43216e5c11e6ad09d3c228771a2b55cdcf49d9
trinitywoods <twoods63@students.kennesaw.edu>
2025-03-09 18:06:59 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+class MyFirstThread implements Runnable {
+    static int next_tid=1;
+    int tid;
+
+    public MyFirstThread() {
+        tid=next_tid++;
+    }
+
+    public void run() {
+        System.out.println("I'm thread "+tid);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        MyFirstThread t1 = new MyFirstThread();
+        MyFirstThread t2 = new MyFirstThread();
+        MyFirstThread t3 = new MyFirstThread();
+        System.out.println("Let's go!");
+        Thread thread1 = new Thread(t1);
+        Thread thread2 = new Thread(t2);
+        Thread thread3 = new Thread(t3);
+        System.out.println("Done");
+    }
+}
